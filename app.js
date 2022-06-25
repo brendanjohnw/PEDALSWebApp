b007d9b151a02137eba948fbf70b505ab6b5cd4b
brendanjohnw <brendanwklee@gmail.com>
2022-06-26 00:06:19 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Listen for requests
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import Handlebars from 'handlebars';
+import { mainRouter } from './routes/mainRouter.js';
+const app = express();
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+// Serve static content
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use('/', mainRouter);
+
+app.listen(process.env.PORT || 3200 || '0.0.0.0', () => {
+    console.log('running!');
+});
---
+++ routes/mainRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { getIndex } from "../controllers/mainController.js";
+
+export const mainRouter = express.Router();
+
+mainRouter.get('/', getIndex);
